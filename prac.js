582dc1bcc5c9746009b9a6ded125cc29a19afe74
TARA <113527198+taradodangeh94@users.noreply.github.com>
2022-09-16 12:42:32 +0430

    practice 1

---
+++ prac.js
@@ -0,0 +1,15 @@
+function NUMBER(num){
+
+if (num==1)console.log("composite num");
+if (num==2)console.log("prime num");
+if (num==3)console.log("prime num");
+if (num==4)console.log("composite num");
+if (num==5)console.log("prime num");
+if (num==6)console.log("composite num");
+if (num==7)console.log("prime num");
+if (num==8)console.log("composite num");
+if (num==9)console.log("composite num");
+if (num==10)console.log("prime num");
+
+}
+NUMBER (9)
